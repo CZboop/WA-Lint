fac91da0f32103805f866505e5d1ad7894ba1d25
Branchey <70140890+CZboop@users.noreply.github.com>
2023-02-20 08:04:57 +0000

    adjusted key value mapping checker to return differences as well as boolean match or not, added new unit tests to test this difference output and adjusted existing unit tests for bool return

--- prototype/keyValReverseCheck.cjs
+++ prototype/keyValReverseCheck.cjs
@@ -8,31 +8,37 @@ class KeyValReverseCheck {
         const keyValPairsInitial = Object.entries(this.initialObj);
         const keyValPairsReverse = Object.entries(this.reverseObj);
 
+        const diffs = [];
+
         let matches = true;
         for (let [key, val] of keyValPairsInitial){
             if (keyValPairsReverse.filter(item => item[0] == val && item[1] == key).length != 1) {
                 matches = false;
-                break;
+                diffs.push({'diff': [key, val]});
+                // break;
+            }
+            else {
+                diffs.push({'same': [key, val]});
             }
         }
         for (let [key, val] of keyValPairsReverse){
             if (keyValPairsInitial.filter(item => item[0] == val && item[1] == key).length != 1) {
                 matches = false;
-                break;
+                diffs.push({'diff': [key, val]});
+                // break;
+            }
+            else {
+                diffs.push({'same': [key, val]});
             }
         }
-        return matches;
-        // TODO: return which ones don't match/diff
-        // if (matches) {
-        //     return {'allMatch': true, 'mismatches' : null};
-        // }
-        // else {
-        //     const diffArray = [];
-        //     // const inInitialNotReverse = ;
-        //     // const inReverseNotInitial = ;
-        //     // do initial, reverse + and -
-        //     return {'allMatch': false, 'mismatches' : []};
-        // }
+        // note, idea is to be able to log the differences in a useful way at later stage to make easier to fix if there's a mismatch
+        // TODO: are there repetitions across first and second object? check and remove if so
+        if (matches) {
+            return {'allMatch': true, 'matches' : diffs};
+        }
+        else {
+            return {'allMatch': false, 'matches' : diffs};
+        }
     }
 }
 
--- prototype/keyValReverseCheck.test.js
+++ prototype/keyValReverseCheck.test.js
@@ -1,11 +1,11 @@
 const {KeyValReverseCheck} = require('./keyValReverseCheck.cjs');
 
-// // // TESTING FUNCTION TO CHECK KEYS AND VALUES ARE REVERSED // // //
+// // // TESTING BOOLEAN OF WHETHER KEYS AND VALUES ARE REVERSED // // //
 
 test('can say that two empty objects reversed', () => {
     let mapOne = {};
     let reverseMap = {};
-    expect(new KeyValReverseCheck(mapOne, reverseMap).check()).toEqual(true);
+    expect(new KeyValReverseCheck(mapOne, reverseMap).check().allMatch).toEqual(true);
 })
 
 test('can say that two objects that match exactly are not the reverse of each other', () => {
@@ -25,7 +25,7 @@ test('can say that two objects that match exactly are not the reverse of each ot
         'key5' : 'penultimate value',
         'key6' : 'last value'
     };
-    expect(new KeyValReverseCheck(mapOne, reverseMap).check()).toEqual(false);
+    expect(new KeyValReverseCheck(mapOne, reverseMap).check().allMatch).toEqual(false);
 })
 
 test('can tell that two short objects are the reverse of each other', () => {
@@ -45,7 +45,7 @@ test('can tell that two short objects are the reverse of each other', () => {
         'penultimate value' : 'key5',
         'last value' : 'key6'
     };
-    expect(new KeyValReverseCheck(mapOne, reverseMap).check()).toEqual(true);
+    expect(new KeyValReverseCheck(mapOne, reverseMap).check().allMatch).toEqual(true);
 })
 
 test('says two objects that almost match reversed, with one difference, are not the reverse of each other', () => {
@@ -65,7 +65,7 @@ test('says two objects that almost match reversed, with one difference, are not 
         'penultimate value' : 'key5',
         'last value' : 'key6'
     };
-    expect(new KeyValReverseCheck(mapOne, reverseMap).check()).toBe(false)
+    expect(new KeyValReverseCheck(mapOne, reverseMap).check().allMatch).toBe(false)
 })
 
 test('can tell objects with same elements reversed, in different order, are the reverse of each other', () => {
@@ -85,5 +85,113 @@ test('can tell objects with same elements reversed, in different order, are the 
         'penultimate value' : 'key5',
         'last value' : 'key6'
     };
-    expect(new KeyValReverseCheck(mapOne, reverseMap).check()).toBe(true);
+    expect(new KeyValReverseCheck(mapOne, reverseMap).check().allMatch).toBe(true);
+})
+
+// // // TESTING DIFF OF WHICH KEYS AND VALUES ARE REVERSED // // //
+
+test('can say that two empty objects reversed', () => {
+    let mapOne = {};
+    let reverseMap = {};
+    expect(new KeyValReverseCheck(mapOne, reverseMap).check().matches).toEqual([]);
+})
+
+test('can say that two objects that match exactly are not the reverse of each other', () => {
+    let mapOne = {
+        'key1' : 'some value',
+        'key2' : 'another value',
+        'key3' : 'value three',
+        'key4' : 'fourth value',
+        'key5' : 'penultimate value',
+        'key6' : 'last value'
+    };
+    let reverseMap = {
+        'key1' : 'some value',
+        'key2' : 'another value',
+        'key3' : 'value three',
+        'key4' : 'fourth value',
+        'key5' : 'penultimate value',
+        'key6' : 'last value'
+    };
+    expect(new KeyValReverseCheck(mapOne, reverseMap).check().matches).toEqual(
+        [{"diff": ["key1", "some value"]}, {"diff": ["key2", "another value"]}, {"diff": ["key3", "value three"]}, 
+        {"diff": ["key4", "fourth value"]}, {"diff": ["key5", "penultimate value"]}, {"diff": ["key6", "last value"]}, 
+        {"diff": ["key1", "some value"]}, {"diff": ["key2", "another value"]}, {"diff": ["key3", "value three"]}, 
+        {"diff": ["key4", "fourth value"]}, {"diff": ["key5", "penultimate value"]}, {"diff":["key6","last value"]}]
+    );
+})
+
+test('can tell that two short objects are the reverse of each other', () => {
+    let mapOne = {
+        'key1' : 'some value',
+        'key2' : 'another value',
+        'key3' : 'value three',
+        'key4' : 'fourth value',
+        'key5' : 'penultimate value',
+        'key6' : 'last value'
+    };
+    let reverseMap = {
+        'some value' : 'key1',
+        'another value' : 'key2',
+        'value three' : 'key3',
+        'fourth value' : 'key4',
+        'penultimate value' : 'key5',
+        'last value' : 'key6'
+    };
+    expect(new KeyValReverseCheck(mapOne, reverseMap).check().matches).toEqual(
+        [{"same": ["key1", "some value"]}, {"same": ["key2", "another value"]}, {"same": ["key3", "value three"]}, 
+        {"same": ["key4", "fourth value"]}, {"same": ["key5", "penultimate value"]}, {"same": ["key6", "last value"]}, 
+        {"same": ["some value", "key1"]}, {"same": ["another value", "key2"]}, {"same": ["value three", "key3"]}, 
+        {"same": ["fourth value", "key4"]}, {"same": ["penultimate value", "key5"]}, {"same":["last value", "key6"]}]
+    );
+})
+
+test('says two objects that almost match reversed, with one difference, are not the reverse of each other', () => {
+    let mapOne = {
+        'key1' : 'some value',
+        'key2' : 'another value',
+        'key3' : 'value three',
+        'key4' : 'fourth value',
+        'key5' : 'penultimate value',
+        'key6' : 'last value'
+    };
+    let reverseMap = {
+        'some value' : 'key0',
+        'another value' : 'key2',
+        'value three' : 'key3',
+        'fourth value' : 'key4',
+        'penultimate value' : 'key5',
+        'last value' : 'key6'
+    };
+    expect(new KeyValReverseCheck(mapOne, reverseMap).check().matches).toEqual(
+        [{"diff": ["key1", "some value"]}, {"same": ["key2", "another value"]}, {"same": ["key3", "value three"]}, 
+        {"same": ["key4", "fourth value"]}, {"same": ["key5", "penultimate value"]}, {"same": ["key6", "last value"]}, 
+        {"diff": ["some value", "key0"]}, {"same": ["another value", "key2"]}, {"same": ["value three", "key3"]}, 
+        {"same": ["fourth value", "key4"]}, {"same": ["penultimate value", "key5"]}, {"same":["last value", "key6"]}]
+    )
+})
+
+test('can tell objects with same elements reversed, in different order, are the reverse of each other', () => {
+    let mapOne = {
+        'key6' : 'last value',
+        'key2' : 'another value',
+        'key1' : 'some value',
+        'key4' : 'fourth value',
+        'key3' : 'value three',
+        'key5' : 'penultimate value'
+    };
+    let reverseMap = {
+        'some value' : 'key1',
+        'another value' : 'key2',
+        'value three' : 'key3',
+        'fourth value' : 'key4',
+        'penultimate value' : 'key5',
+        'last value' : 'key6'
+    };
+    expect(new KeyValReverseCheck(mapOne, reverseMap).check().matches).toEqual(
+        [{"same": ["key6", "last value"]}, {"same": ["key2", "another value"]}, {"same": ["key1", "some value"]}, 
+        {"same": ["key4", "fourth value"]}, {"same": ["key3", "value three"]}, {"same": ["key5", "penultimate value"]}, 
+        {"same": ["some value", "key1"]}, {"same": ["another value", "key2"]}, {"same": ["value three", "key3"]}, 
+        {"same": ["fourth value", "key4"]}, {"same": ["penultimate value", "key5"]}, {"same":["last value", "key6"]}]
+    );
 })
