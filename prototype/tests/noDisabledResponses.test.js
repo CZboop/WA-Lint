3b92b6e72d02191dd9128c8897df477231d67edc
Branchey <70140890+CZboop@users.noreply.github.com>
2023-05-21 09:55:55 +0100

    added unit tests for returning ids of disabled responses

--- prototype/tests/noDisabledResponses.test.js
+++ prototype/tests/noDisabledResponses.test.js
@@ -126,3 +126,125 @@ test('workspace with no responses at all returns true in boolean', () => {
 })
 
 // // // TESTING CORRECT LIST OF RESPONSE NODES RETURNED // // //
+
+test('workspace with no response type nodes returns empty array of node ids', () => {
+    let testSkill = {"name": "test skill", "intents": [{"intent": "first_intent", "examples": []}, {"intent": "third_intent", "examples": []}, {"intent": "second_intent", "examples": []}], "dialog_nodes": [
+        {
+        "type": "standard",
+        "title": "A node",
+        "output": {"text": {"values": ["Some text"], "selection_policy": "sequential"}},
+        "metadata": {},
+        "conditions": "true",
+        "dialog_node": "Opening"},
+        {
+        "type": "standard",
+        "title": "The node",
+        "output": {"text": {"values": ["A response"], "selection_policy": "sequential"}},
+        "metadata": {},
+        "conditions": "false",
+        "dialog_node": "Opening"},
+        {
+        "type": "standard",
+        "title": "Node",
+        "output": {"text": {"values": ["It's a response"], "selection_policy": "sequential"}},
+        "metadata": {},
+        "conditions": "true",
+        "dialog_node": "Opening"}]};
+
+    expect(new NoDisabledResponses(testSkill).run().nodeIds).toEqual([]);
+})
+
+test('workspace with no disabled response type nodes returns empty array of node ids', () => {
+    let testSkill = {"name": "test skill", "intents": [{"intent": "first_intent", "examples": []}, {"intent": "third_intent", "examples": []}, {"intent": "second_intent", "examples": []}], "dialog_nodes": [
+        {
+        "type": "response_condition",
+        "title": "A node",
+        "output": {"text": {"values": ["Some text"], "selection_policy": "sequential"}},
+        "context": {"a_context_var" : "a value",
+            "test_var": "value if from another node"},
+        "metadata": {},
+        "conditions": "true",
+        "dialog_node": "node_1212"},
+        {
+        "type": "response_condition",
+        "title": "The node",
+        "output": {"text": {"values": ["A response"], "selection_policy": "sequential"}},
+        "context": {"test_var" : "value if from this node",
+            "needle": "search complete"},
+        "metadata": {},
+        "conditions": "false",
+        "dialog_node": "response_1432"},
+        {
+        "type": "standard",
+        "title": "Node",
+        "output": {"text": {"values": ["It's a response"], "selection_policy": "sequential"}},
+        "context": {"placeholder" : null,
+            "test_var": "another value"},
+        "metadata": {},
+        "conditions": "$maybe == true",
+        "dialog_node": "Opening"}]};
+
+    expect(new NoDisabledResponses(testSkill).run().nodeIds).toEqual([]);
+})
+
+test('workspace with one disabled response type nodes returns array of that node id', () => {
+    let testSkill = {"name": "test skill", "intents": [{"intent": "first_intent", "examples": []}, {"intent": "third_intent", "examples": []}, {"intent": "second_intent", "examples": []}], "dialog_nodes": [
+        {
+        "type": "response_condition",
+        "title": "A node",
+        "output": {"text": {"values": ["Some text"], "selection_policy": "sequential"}},
+        "context": {"a_context_var" : "a value",
+            "test_var": "value if from another node"},
+        "disabled" : true,
+        "metadata": {},
+        "conditions": "true",
+        "dialog_node": "node_1212"},
+        {
+        "type": "response_condition",
+        "title": "The node",
+        "output": {"text": {"values": ["A response"], "selection_policy": "sequential"}},
+        "context": {"test_var" : "value if from this node",
+            "needle": "search complete"},
+        "metadata": {},
+        "conditions": "false",
+        "dialog_node": "response_1432"},
+        {
+        "type": "standard",
+        "title": "Node",
+        "output": {"text": {"values": ["It's a response"], "selection_policy": "sequential"}},
+        "metadata": {},
+        "conditions": "$maybe == true",
+        "dialog_node": "Opening"}]};
+
+    expect(new NoDisabledResponses(testSkill).run().nodeIds).toEqual(['node_1212']);
+})
+
+test('workspace with multiple disabled response type nodes returns array of all those node ids', () => {
+    let testSkill = {"name": "test skill", "intents": [{"intent": "first_intent", "examples": []}, {"intent": "third_intent", "examples": []}, {"intent": "second_intent", "examples": []}], "dialog_nodes": [
+        {
+        "type": "response_condition",
+        "title": "A node",
+        "output": {"text": {"values": ["Some text"], "selection_policy": "sequential"}},
+        "disabled" : true,
+        "metadata": {},
+        "conditions": "true",
+        "dialog_node": "node_1212"},
+        {
+        "type": "response_condition",
+        "title": "The node",
+        "output": {"text": {"values": ["A response"], "selection_policy": "sequential"}},
+        "disabled" : true,
+        "metadata": {},
+        "conditions": "false",
+        "dialog_node": "response_1432"},
+        {
+        "type": "response_condition",
+        "title": "Node",
+        "output": {"text": {"values": ["It's a response"], "selection_policy": "sequential"}},
+        "disabled" : true,
+        "metadata": {},
+        "conditions": "$maybe == true",
+        "dialog_node": "response_1875"}]};
+
+    expect(new NoDisabledResponses(testSkill).run().nodeIds.sort()).toEqual(["response_1432", "node_1212", "response_1875"].sort());
+})
