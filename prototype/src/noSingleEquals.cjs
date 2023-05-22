3619142d5027c0163f4bdf19d6fe091df3fc6cf5
Branchey <70140890+CZboop@users.noreply.github.com>
2023-05-22 07:49:59 +0100

    added start of logic for check for single equals in node condition and unit tests for boolean return from this

---
+++ prototype/src/noSingleEquals.cjs
@@ -0,0 +1,25 @@
+class NoSingleEquals {
+    constructor (skill) {
+        this.skill = skill;
+    }
+    run(){
+        let singleEqualsInConditions = this.findSingleEqualsInConditions();
+        return {'bool' : singleEqualsInConditions.length === 0, 'nodes' : [singleEqualsInConditions]};
+    }
+    findSingleEqualsInConditions(){
+        // TODO: handle multiple conditions for one node
+        let singleEqualsInConditions = this.skill['dialog_nodes'].filter(node => {return node.conditions.includes("=") && !node.conditions.includes("==")});
+        console.log(singleEqualsInConditions);
+        return singleEqualsInConditions;
+    }
+    // findSingleEqualsInOutput(){
+    //     // TODO: handle multiple outputs for one node
+    //     // TODO: get to the actual output within the node, is this always the same? i.e. always generic? and need to map objects and arrays etc.
+    //     let singleEqualsInOutput = this.this.skill['dialog_nodes'].filter(node => {return node.output.generic.contains("=") && !node.output.generic.contains("==")});
+    //     return singleEqualsInOutput;
+    // }
+}
+
+module.exports = {
+    NoSingleEquals
+}
---
+++ prototype/tests/noSingleEquals.test.js
@@ -0,0 +1,179 @@
+const {NoSingleEquals} = require('../src/noSingleEquals.cjs');
+
+// // // TESTING BOOLEAN IN NODE CONDITIONS // // //
+
+test('skill with double equals but no single equals in node conditions returns true', () => {
+    let testSkill = {"name": "test skill", "intents": [{"intent": "true_intent", "examples": []}, {"intent": "first_intent", "examples": []}, {"intent": "third_intent", "examples": []}, {"intent": "second_intent", "examples": []}], "dialog_nodes": [
+        {
+        "type": "standard",
+        "title": "A node",
+        "output": {"text": {"values": ["Some text", "Some more text"], "selection_policy": "multiline"}},
+        "context": {"a_context_var" : "a value",
+            "test_var": "value if from another node"},
+        "metadata": {},
+        "conditions": "$intent_name==\"first_intent\"",
+        "dialog_node": "Opening"},
+        {
+        "type": "standard",
+        "title": "A node",
+        "output": {"text": {"values": ["Some text"], "selection_policy": "sequential"}},
+        "context": {"a_context_var" : "a value",
+            "test_var": "value if from another node"},
+        "metadata": {},
+        "conditions": "$intent_name==\"second_intent\"",
+        "dialog_node": "Opening"},
+        {
+        "type": "standard",
+        "title": "A node",
+        "output": {"text": {"values": ["Some text"], "selection_policy": "sequential"}},
+        "context": {"a_context_var" : "a value",
+            "test_var": "value if from another node"},
+        "metadata": {},
+        "conditions": "$intent_name==\"third_intent \"",
+        "dialog_node": "Opening"},
+        {
+        "type": "standard",
+        "title": "The node",
+        "output": {"text": {"values": ["A response", "Another response"], "selection_policy": "multiline"}},
+        "context": {"test_var" : "value if from this node",
+            "needle": "search complete"},
+        "metadata": {},
+        "conditions": "true",
+        "dialog_node": "Opening"},
+        {
+        "type": "standard",
+        "title": "Node",
+        "output": {"text": {"values": ["It's a response"], "selection_policy": "sequential"}},
+        "context": {"placeholder" : null,
+            "test_var": "another value"},
+        "metadata": {},
+        "conditions": "$intent_name==\"true_intent\"",
+        "dialog_node": "Opening"}]};
+
+    expect(new NoSingleEquals(testSkill).run().bool).toBe(true);
+})
+
+test('skill with one single equals in node conditions returns false', () => {
+    let testSkill = {"name": "test skill", "intents": [{"intent": "true_intent", "examples": []}, {"intent": "first_intent", "examples": []}, {"intent": "third_intent", "examples": []}, {"intent": "second_intent", "examples": []}], "dialog_nodes": [
+        {
+        "type": "standard",
+        "title": "A node",
+        "output": {"text": {"values": ["Some text", "Some more text"], "selection_policy": "multiline"}},
+        "context": {"a_context_var" : "a value",
+            "test_var": "value if from another node"},
+        "metadata": {},
+        "conditions": "$intent_name =\"first_intent\"",
+        "dialog_node": "Opening"},
+        {
+        "type": "standard",
+        "title": "A node",
+        "output": {"text": {"values": ["Some text"], "selection_policy": "sequential"}},
+        "context": {"a_context_var" : "a value",
+            "test_var": "value if from another node"},
+        "metadata": {},
+        "conditions": "$intent_name==\"second_intent\"",
+        "dialog_node": "Opening"},
+        {
+        "type": "standard",
+        "title": "A node",
+        "output": {"text": {"values": ["Some text"], "selection_policy": "sequential"}},
+        "context": {"a_context_var" : "a value",
+            "test_var": "value if from another node"},
+        "metadata": {},
+        "conditions": "$intent_name==\"third_intent \"",
+        "dialog_node": "Opening"},
+        {
+        "type": "standard",
+        "title": "The node",
+        "output": {"text": {"values": ["A response", "Another response"], "selection_policy": "multiline"}},
+        "context": {"test_var" : "value if from this node",
+            "needle": "search complete"},
+        "metadata": {},
+        "conditions": "true",
+        "dialog_node": "Opening"},
+        {
+        "type": "standard",
+        "title": "Node",
+        "output": {"text": {"values": ["It's a response"], "selection_policy": "sequential"}},
+        "context": {"placeholder" : null,
+            "test_var": "another value"},
+        "metadata": {},
+        "conditions": "$intent_name==\"true_intent\"",
+        "dialog_node": "Opening"}]};
+
+    expect(new NoSingleEquals(testSkill).run().bool).toBe(false);
+})
+
+test('skill with multiple single equals in node conditions returns false', () => {
+    let testSkill = {"name": "test skill", "intents": [{"intent": "true_intent", "examples": []}, {"intent": "first_intent", "examples": []}, {"intent": "third_intent", "examples": []}, {"intent": "second_intent", "examples": []}], "dialog_nodes": [
+        {
+        "type": "standard",
+        "title": "A node",
+        "output": {"text": {"values": ["Some text", "Some more text"], "selection_policy": "multiline"}},
+        "context": {"a_context_var" : "a value",
+            "test_var": "value if from another node"},
+        "metadata": {},
+        "conditions": "$intent_name==\"first_intent\"",
+        "dialog_node": "Opening"},
+        {
+        "type": "standard",
+        "title": "A node",
+        "output": {"text": {"values": ["Some text"], "selection_policy": "sequential"}},
+        "context": {"a_context_var" : "a value",
+            "test_var": "value if from another node"},
+        "metadata": {},
+        "conditions": "$intent_name = \"second_intent\"",
+        "dialog_node": "Opening"},
+        {
+        "type": "standard",
+        "title": "A node",
+        "output": {"text": {"values": ["Some text"], "selection_policy": "sequential"}},
+        "context": {"a_context_var" : "a value",
+            "test_var": "value if from another node"},
+        "metadata": {},
+        "conditions": "$intent_name=\"third_intent \"",
+        "dialog_node": "Opening"},
+        {
+        "type": "standard",
+        "title": "The node",
+        "output": {"text": {"values": ["A response", "Another response"], "selection_policy": "multiline"}},
+        "context": {"test_var" : "value if from this node",
+            "needle": "search complete"},
+        "metadata": {},
+        "conditions": "true",
+        "dialog_node": "Opening"},
+        {
+        "type": "standard",
+        "title": "Node",
+        "output": {"text": {"values": ["It's a response"], "selection_policy": "sequential"}},
+        "context": {"placeholder" : null,
+            "test_var": "another value"},
+        "metadata": {},
+        "conditions": "$intent_name = \"true_intent\"",
+        "dialog_node": "Opening"}]};
+
+    expect(new NoSingleEquals(testSkill).run().bool).toBe(false);
+})
+
+test('skill with no equals in node conditions returns true', () => {
+    let testSkill = {"name": "test skill", "intents": [{"intent": "true_intent", "examples": []}, {"intent": "first_intent", "examples": []}, {"intent": "third_intent", "examples": []}, {"intent": "second_intent", "examples": []}], "dialog_nodes": [{
+        "type": "standard",
+        "title": "The node",
+        "output": {"text": {"values": ["A response", "Another response"], "selection_policy": "multiline"}},
+        "context": {"test_var" : "value if from this node",
+            "needle": "search complete"},
+        "metadata": {},
+        "conditions": "false",
+        "dialog_node": "Opening"},
+        {
+        "type": "standard",
+        "title": "Node",
+        "output": {"text": {"values": ["It's a response"], "selection_policy": "sequential"}},
+        "context": {"placeholder" : null,
+            "test_var": "another value"},
+        "metadata": {},
+        "conditions": "true",
+        "dialog_node": "Opening"}]};
+
+    expect(new NoSingleEquals(testSkill).run().bool).toBe(true);
+})
